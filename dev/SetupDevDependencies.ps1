c941229a843373e69cd4fed8034ec71f02b6f173
Andrew <amhector@stanford.edu>
2023-09-08 06:36:49 -0700

    build: Add explicit check for virtual environment in dev setup script

--- dev/SetupDevDependencies.ps1
+++ dev/SetupDevDependencies.ps1
@@ -1,7 +1,7 @@
 # This script will use path variables determined by the current virtual environment (default) or by PATH
 #requires -version 5.1
 $ErrorActionPreference = "Stop"
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip --require-virtualenv
 pip install poetry
 poetry install --with dev
 pre-commit install
--- dev/setup_dev_dependencies.sh
+++ dev/setup_dev_dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This script will use current path variables. Ensure you are in a virtual environment.
 # For Ubuntu: This script will throw an error if run outside a virtual environment.
-python -m pip install --upgrade pip &&
+python -m pip install --upgrade pip --require-virtualenv &&
 pip install poetry &&
 poetry install --with dev &&
 pre-commit install &&
